bbd4aa2e85fb90d584c0c195c10c26fa19d2a7b1
Rubai Rahman <rubairahman1@gmail.com>
2025-09-03 15:09:52 +0600

    initial table created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
-import { Button } from '@/components/ui/button';
+import { columns, User } from '@/components/users/columns';
+import { DataTable } from '@/components/users/data-table';
 
-export default function Home() {
+async function getData(): Promise<User[]> {
+  // Fetch data from your API here.
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  return data;
+}
+
+export default async function HomePage() {
+  const data = await getData();
+  console.log('data', data);
+
   return (
-    <div>
-      <h1>Home</h1>
-      <Button>Hello</Button>
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
---
+++ src/components/users/columns.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import { ColumnDef } from '@tanstack/react-table';
+import { MoreHorizontal } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { ArrowUpDown } from 'lucide-react';
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
+export const columns: ColumnDef<User>[] = [
+  {
+    accessorKey: 'id',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          ID
+          <ArrowUpDown className="ml-2 h-4 w-4 shrink-0" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="text-start">{row.index + 1}</div>,
+  },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4 shrink-0" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="text-start">{row.original.name}</div>,
+  },
+  {
+    accessorKey: 'email',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4 shrink-0" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="text-start">{row.original.email}</div>,
+  },
+  {
+    accessorKey: 'phone',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Phone
+          <ArrowUpDown className="ml-2 h-4 w-4 shrink-0" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="text-start">{row.original.phone}</div>,
+  },
+  {
+    accessorKey: 'company',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Company
+          <ArrowUpDown className="ml-2 h-4 w-4 shrink-0" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="text-start">{row.original.company.name}</div>
+    ),
+  },
+];
